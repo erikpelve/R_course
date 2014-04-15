2c77bd9c13f872d6e281bd35ca6306312dd50c56
erikpelve <erik.pelve@gmail.com>
2014-04-15 12:28:33 -0400

    Update Corr.R
    framework, need to be reworked

--- Corr.R
+++ Corr.R
@@ -4,24 +4,39 @@
 corr <- function(x, y) {
 
   directory <-x
+  threshold <- y
+  files <- list.files(x) 
+   #length(files)
+  
+  results <-data.frame(sulfate = numeric(0), nitrate = numeric(0))
 
-  results <-data.frame(id = numeric(0), nobs = numeric(0))
   #Find each file name, read each file
-  for(n in 1:length(id)){
-     #add initial zeroes
-     counter <-0
-     if (nchar(id[n]) == 1){ id2<- paste(c("00"), id[n], sep = "")}
-     if (nchar(id[n]) == 2){ id2<- paste(c("0"), id[n], sep = "")}
-     if (nchar(id[n]) == 3){ id2<-id[n]}
-     file <- paste(id2, c("csv"), sep = ".")  #make a full filename
+
+
+  for(n in 1:length(files)){
+     counter = 0
+	 file <- files[n]
+	 #file <- paste(id2, c("csv"), sep = ".")  #make a full filename
      path <-paste(directory, file, sep="/") #put together the file path
      table <-read.csv(path, sep=",") #read file
+	sample_results <-data.frame(sulfate = numeric(0), nitrate = numeric(0))
+	for(o in 1:nrow(table)){
+       if(is.na(table[o,2]) == FALSE && is.na(table[o,3]) == FALSE){
+	#		thisid <- (table[o,4])
+			counter = counter +1
+			thisresult <-data.frame(sulfate = table[o,2], nitrate = table[o,3] )
+			sample_results <- rbind(sample_results, thisresult)
+		
+		}
+	}	
+
+	 #For files where the number of complete observations is greater than the treshhold
+	 if (counter > 0){
+	 	 results <- rbind(results,sample_result)
+	 
+	 }
 
 
-}
-
-
-
-
+	print(results)
 
 }
