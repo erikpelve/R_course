8a8a67e795d6bc03cb2ea226b7bca3fb22e8fee5
erikpelve <erik.pelve@gmail.com>
2014-04-15 05:19:04 -0400

    Update Complete.R
    Ready to test

--- Complete.R
+++ Complete.R
@@ -1,7 +1,5 @@
 #Write a function that reads a directory full of files and reports the number of completely observed cases in each data file. The function should return a data frame where the first column is the name of the file and the second column is the number of complete cases. A prototype of this function follows
-
 #R
-
 
 #complete <- function(directory, id = 1:332) {
 complete <- function(x, y){
@@ -10,10 +8,12 @@ complete <- function(x, y){
 directory <-x
 id <-y #vector with one or several numbers
 
+counter <-0
 
 #Find each file name, read each file
 for(n in 1:length(id)){
      #add initial zeroes
+     counter <-0
      if (nchar(id[n]) == 1){ id2<- paste(c("00"), id[n], sep = "")}
      if (nchar(id[n]) == 2){ id2<- paste(c("0"), id[n], sep = "")}
      if (nchar(id[n]) == 3){ id2<-id[n]}
@@ -21,9 +21,11 @@ for(n in 1:length(id)){
      path <-paste(directory, file, sep="/") #put together the file path
      table <-read.csv(path, sep=",") #read file
      
-     
-
-
-
-
+     for(n in 1:ncol(table)){
+          if(table[1,2] != "NA") & (table[1,3] != "NA"){
+               counter = counter +1
+     }
+     return(file, counter)
 }
+
+#table <-read.csv(specdata/001.csv, sep=",")     
