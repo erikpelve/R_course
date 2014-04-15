a0a844b13b879926ea3df645745b6a4ac9ba5cbc
erikpelve <erik.pelve@gmail.com>
2014-04-15 14:10:15 -0400

    Update Corr.R
    It works to a point, but the output is wrong. Have to read up on the cor function

--- Corr.R
+++ Corr.R
@@ -10,33 +10,44 @@ corr <- function(x, y) {
   
   results <-data.frame(sulfate = numeric(0), nitrate = numeric(0))
 
-  #Find each file name, read each file
+  
+#My own function from previous, run for all files
+output_complete <- complete(directory, 1:length(files))
+#While I test, I only run the first 10 samples
 
+#output_complete <- complete(directory, 1:10)
 
-  for(n in 1:length(files)){
-     counter = 0
-	 file <- files[n]
-	 #file <- paste(id2, c("csv"), sep = ".")  #make a full filename
-     path <-paste(directory, file, sep="/") #put together the file path
-     table <-read.csv(path, sep=",") #read file
-	sample_results <-data.frame(sulfate = numeric(0), nitrate = numeric(0))
-	for(o in 1:nrow(table)){
-       if(is.na(table[o,2]) == FALSE && is.na(table[o,3]) == FALSE){
-	#		thisid <- (table[o,4])
-			counter = counter +1
-			thisresult <-data.frame(sulfate = table[o,2], nitrate = table[o,3] )
-			sample_results <- rbind(sample_results, thisresult)
-		
+ #For each line in the output 
+for(n in 1:nrow(output_complete)){
+	id <- output_complete[n,1]
+	no_complete <- output_complete[n,2]
+	#For files where the number of complete observations is greater 	than the threshold
+	 if (no_complete > threshold){
+	   sample_results <-data.frame(sulfate = numeric(0), nitrate = numeric(0))
+	 	 #open file
+		if (nchar(id) == 1){ id2<- paste(c("00"), id, sep = "")}
+		if (nchar(id) == 2){ id2<- paste(c("0"), id, sep = "")}
+		if (nchar(id) == 3){ id2<-id}
+		file <- paste(id2, c("csv"), sep = ".")  #make a full filename
+		path <-paste(directory, file, sep="/") #put together the file path
+		table <-read.csv(path, sep=",") #read file
+		#read through file and collect values for sulfate and nitrate
+		for(o in 1:nrow(table)){
+			if(is.na(table[o,2]) == FALSE && is.na(table[o,3]) == FALSE){
+				thisresult <- data.frame(sulfate = table[o,2], nitrate = table[o,3] )
+				sample_results <- rbind(sample_results, thisresult)
+				}
 		}
-	}	
+		
+		#Here I collect all datapoints with both sulfate and nitrate data from stations over the treshhold 
+		 results <- rbind(results,sample_results)
+	}
+}	
 
-	 #For files where the number of complete observations is greater than the treshhold
-	 if (counter > 0){
-	 	 results <- rbind(results,sample_result)
+	correlated_results <- cor(results)
+	return(correlated_results)
 	 
-	 }
-
-
-	print(results)
-
 }
+
+
+
